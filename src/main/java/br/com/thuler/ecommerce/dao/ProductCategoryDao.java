c57b5ac70188326d3b7ff165296fc16151e2b797
Henry Thuler <henrythuler2003@outlook.com>
2024-06-21 11:20:26 -0300

    feat(cors): adding cross origin for repositories

--- src/main/java/br/com/thuler/ecommerce/dao/ProductCategoryDao.java
+++ src/main/java/br/com/thuler/ecommerce/dao/ProductCategoryDao.java
@@ -3,7 +3,9 @@ package br.com.thuler.ecommerce.dao;
 import br.com.thuler.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryDao extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/br/com/thuler/ecommerce/dao/ProductDao.java
+++ src/main/java/br/com/thuler/ecommerce/dao/ProductDao.java
@@ -2,6 +2,8 @@ package br.com.thuler.ecommerce.dao;
 
 import br.com.thuler.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductDao extends JpaRepository<Product, Long> {
 }
