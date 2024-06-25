9b7047d1ac994f9746effe0c24b3ab249d7cdc3b
Henry Thuler <henrythuler2003@outlook.com>
2024-06-25 09:55:23 -0300

    refactor(app): Returning entities ids and new search method implemented

--- src/main/java/br/com/thuler/ecommerce/config/DataRestConfig.java
+++ src/main/java/br/com/thuler/ecommerce/config/DataRestConfig.java
@@ -2,14 +2,24 @@ package br.com.thuler.ecommerce.config;
 
 import br.com.thuler.ecommerce.entity.Product;
 import br.com.thuler.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+
+    @Autowired
+    private EntityManager entityManager;
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -26,6 +36,22 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                 .withItemExposure((metadata, httpMethods) -> httpMethods.disable(methods))
                 .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(methods));
 
+        exposeIds(config);
+
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config){
+
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        for(EntityType tempEntityType: entities){
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 
 }
--- src/main/java/br/com/thuler/ecommerce/dao/ProductDao.java
+++ src/main/java/br/com/thuler/ecommerce/dao/ProductDao.java
@@ -1,9 +1,15 @@
 package br.com.thuler.ecommerce.dao;
 
 import br.com.thuler.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
 public interface ProductDao extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@Param("id") Long category, Pageable pageable);
+
 }
